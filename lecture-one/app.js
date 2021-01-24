066ec71f352de5baa6f510678df4b84f330c9a76
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-24 22:21:44 +0600

    added some line in lecture one

--- lecture-one/app.js
+++ lecture-one/app.js
@@ -1,2 +1,18 @@
-console.log('this very easy to learning javascript');
-console.log('this is easy to learnign javascript');
+// using id name
+
+let title = document.getElementById('title');
+
+// using the className
+let paragraph = document.getElementsByClassName('lead');
+
+for (let i = 0; i < paragraph.length; i++) {
+  let element = paragraph[i];
+  element.style.color = 'red';
+  element.style.backgroundColor = '#eee';
+  element.style.padding = '10px';
+}
+
+// using the tagName
+
+let list = document.getElementsByTagName('li');
+console.log(list);
