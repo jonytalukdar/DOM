3ec6b1549a212bfb6ddd375c07d9937c96c5b7a5
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-16 21:47:21 +0600

    added some line in main folder

---
+++ main/app.js
@@ -0,0 +1,21 @@
+//  inseradjacentment
+// const li = document.createElement('li');
+// li.classList = 'list-group-item text-center';
+// li.innerHTML = '<h1> hello </h1>';
+
+// const items = document.getElementById('items');
+// items.insertAdjacentElement('beforebegin', li);
+
+// const firstItem = document.getElementById('list-one');
+// setTimeout(() => {
+//   firstItem.innerText = 'First Item has been modified';
+//   firstItem.style.background = 'red';
+// }, 5000);
+
+// setTimeout(() => {
+// //   firstItem.remove();
+// }, 5000);
+
+// setInterval(() => {
+//   firstItem.innerText = Math.random();
+// }, 2000);
